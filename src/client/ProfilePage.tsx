166f341895464026c02a31cc67b43871fd9d2502
Matteo Kinkela <mkinkela123@gmail.com>
2023-05-06 13:27:41 +0200

    update user data

--- src/client/ProfilePage.tsx
+++ src/client/ProfilePage.tsx
@@ -1,12 +1,18 @@
 import { useQuery } from "@wasp/queries";
 import getMe from "@wasp/queries/getMe";
-import { PhotoIcon, PlusIcon, TrophyIcon } from "@heroicons/react/24/outline";
+import {
+  PencilSquareIcon,
+  PhotoIcon,
+  PlusIcon,
+  TrophyIcon
+} from "@heroicons/react/24/outline";
 import uploadProfilePic from "@wasp/actions/uploadProfilePic";
 import uploadFeaturedImage from "@wasp/actions/uploadFeaturedImage";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import UploadPostModal from "./components/UploadPostModal";
 import { withPage } from "./components/withPage";
 import { ProfileSkeleton } from "./components/ProfileSkeleton";
+import updateUser from "@wasp/actions/updateUser";
 
 interface IGetMe {
   featuredImage: { imageUrl: string };
@@ -30,6 +36,10 @@ interface IGetMe {
 const ProfilePage = () => {
   const { data: user, isLoading, refetch } = useQuery<any, IGetMe>(getMe);
   const [showModal, setShowModal] = useState<boolean>(false);
+  const [updateUserData, setUpdateUserData] = useState<boolean>(false);
+  const firstNameRef = useRef<HTMLInputElement>(null);
+  const lastNameRef = useRef<HTMLInputElement>(null);
+  const bioRef = useRef<HTMLTextAreaElement>(null);
 
   const getFullName = () =>
     `${user?.userData?.firstName ?? "John"} ${
@@ -73,6 +83,22 @@ const ProfilePage = () => {
     }
   };
 
+  const onSave = async () => {
+    try {
+      await updateUser({
+        firstName: firstNameRef.current?.value,
+        lastName: lastNameRef.current?.value,
+        bio: bioRef.current?.value
+      });
+    } catch {
+      console.error("Could not update user");
+    } finally {
+      onClose();
+    }
+  };
+
+  const onClose = () => setUpdateUserData(false);
+
   return isLoading ? (
     <ProfileSkeleton />
   ) : (
@@ -105,8 +131,82 @@ const ProfilePage = () => {
             />
           </div>
           <div>
-            <h1 className="text-2xl font-bold">{getFullName()}</h1>
-            <p className="text-gray-600">{user?.userData?.bio}</p>
+            <div className="flex">
+              {updateUserData ? (
+                <div className="flex flex-col">
+                  <div className="flex items-center gap-2">
+                    <div className="flex flex-col">
+                      <label
+                        htmlFor="firstName"
+                        className="mb-2 block font-bold"
+                      >
+                        First name
+                      </label>
+                      <input
+                        ref={firstNameRef}
+                        type="text"
+                        id="firstName"
+                        className="border-gray-300 w-full rounded-md border px-4 py-2"
+                      />
+                    </div>
+                    <div className="flex flex-col">
+                      <label
+                        htmlFor="lastName"
+                        className="mb-2 block font-bold"
+                      >
+                        Last name
+                      </label>
+                      <input
+                        ref={lastNameRef}
+                        type="text"
+                        id="lastName"
+                        className="border-gray-300 w-full rounded-md border px-4 py-2"
+                      />
+                    </div>
+                  </div>
+                  <div className="my-4">
+                    <label htmlFor="bio" className="mb-2 block font-bold">
+                      Bio
+                    </label>
+                    <textarea
+                      ref={bioRef}
+                      id="bio"
+                      rows={5}
+                      className="border-gray-300 w-full rounded-md border px-4 py-2"
+                    />
+                  </div>
+                  <div className="flex gap-2 p-4">
+                    <button
+                      onClick={onClose}
+                      className="text-black flex w-full items-center justify-center space-x-2 rounded-md border border-primary bg-white px-4 py-2 focus:outline-none"
+                    >
+                      Cancel
+                    </button>
+                    <button
+                      onClick={onSave}
+                      className="text-black flex w-full items-center justify-center space-x-2 rounded-md bg-primary px-4 py-2 focus:outline-none"
+                    >
+                      Post
+                    </button>
+                  </div>
+                </div>
+              ) : (
+                <div className="flex flex-col">
+                  <div className="flex">
+                    <h1 className="text-2xl font-bold">{getFullName()}</h1>
+                    <div
+                      onClick={() => setUpdateUserData(true)}
+                      className="ml-2 flex items-center justify-center rounded-full border bg-white p-1.5 hover:bg-primary"
+                    >
+                      <PencilSquareIcon className="h-5 w-5 text-gray-900" />
+                    </div>
+                  </div>
+                  <div className="my-2 flex items-center">
+                    <p className="text-gray-600">{user?.userData?.bio}</p>
+                  </div>
+                </div>
+              )}
+            </div>
             <div className="flex gap-2">
               <div>
                 <strong>Posts: </strong> {0}
